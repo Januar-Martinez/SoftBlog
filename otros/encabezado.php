091370a69553e697eb6bf34027f55382787f993c
Januar-Martinez <januarpalacios5@gmail.com>
2021-11-16 11:35:45 -0500

    Reestructuración del menú

--- otros/encabezado.php
+++ otros/encabezado.php
@@ -8,11 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SoftBlog</title>
+    <link rel="stylesheet" href="../css/estilos.css">
     <link rel="icon" type="image/png" href="../img/favicon.png">
-    <link rel="stylesheet" href="../css/master.css">
-    <link rel="stylesheet" href="../css/estyle.css">
-    <link rel="stylesheet" href="../css/estyleT.css">
-    <link rel="stylesheet" href="../css/estiloM.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
@@ -20,32 +17,21 @@
 //var_dump ($_SESSION);
     if(isset($_SESSION['emailUsuario'])):
         echo"
-
-            <nav>
-                <input type='checkbox' id='check'>
-                <label for='check' class='checkbtn'>
-                    <i class='fas fa-bars'></i>
-                </label>
-                <a href='#' class='enlace'>
-                    <h1 style='color:#b80f22; margin-top:20px;'>SoftBlog</h1>
-                </a>
-                <ul>
-                    <li><a class='active' href='../page/inicio.php'>Inicio</a></li>
-                    <li><a href='../page/pregunta.php'>Nueva Preguntas</a></li>
-                </ul>
-            </nav>
-
-            <div class='wrapper'>
-                <div class='sidebar'>
-                    <h2>";?> <?= $_SESSION['usuario']?> <?php echo" </h2>
-                    <h6>";?> <?= $_SESSION['emailUsuario']?> <?php echo"</h6>
-                    <div class='social_media'>
-                        <a href='../otros/cerrarSesion.php'><i class='fas fa-sign-out-alt'></i></i></a>
-                    </div>
-                </div>
-            
-                <div class='main_content'>
-                    <div class='info'>
+        <nav>
+            <input type='checkbox' id='check'>
+            <label for='check' class='checkbtn'>
+                <i class='fas fa-bars'></i>
+            </label>
+            <a href='#' class='enlace'>
+                <h1>SoftBlog</h1>
+            </a>
+            <ul>
+                <li><a class='' href='../page/inicio.php'>Inicio</a></li>
+                <li><a href='../page/pregunta.php'>Nueva Preguntas</a></li>
+                <li><a href='../otros/cerrarSesion.php'><i class='fas fa-sign-out-alt'></i></i></a></li>
+            </ul>
+        </nav>
+        <div>
         ";
     else:
 		echo "
